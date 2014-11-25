f278e99e3446ae3507dfa9aeb81698bc22afeab2
Frank Wiebenga <atrueresistance@gmail.compwd>
2014-11-25 13:12:34 -0600

    matplotlib build from source yosemite

---
+++ CH02-Numpy/data_plot.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python
+from __future__ import print_function
+from __future__ import division
+from __future__ import absolute_import
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+# create x, randomly spaced between 0 to 20
+x = np.linspace(0,20,10)
