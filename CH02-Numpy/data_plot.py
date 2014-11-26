986b4281b3ae1083fcc96e9a4f080b692761a374
Frank Wiebenga <atrueresistance@gmail.compwd>
2014-11-25 18:29:11 -0600

    sal plot

--- CH02-Numpy/data_plot.py
+++ CH02-Numpy/data_plot.py
@@ -7,5 +7,18 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 # create x, randomly spaced between 0 to 20
-x = np.linspace(0,20,10)
-print(x)
+x = np.linspace(0,20,1000)
+
+y1 = np.sin(x)
+y2 = np.cos(x)
+
+#Plot the sin and cos functions
+plt.plot(x, y1, "-g", label="sin")
+plt.plot(x, y2, "-b", label="cos")
+
+#Legend top right
+plt.legend(loc="upper right")
+
+#Limit the y axis to -1.5 to 1.5
+plt.ylim(-1.5, 1.5)
+plt.show()
